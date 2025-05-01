56ed2a9361417c1ea4c9c8cab8de7cd2c7909f68
MiguelSotoD <23soto330@gmail.com>
2025-05-01 14:39:37 -0600

    navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-     <SignedOut>
-        <SignInButton>
-          <Button>Sign up</Button>
-        </SignInButton>
-
-        <SignUpButton />
-    </SignedOut>
-    <SignedIn>
-        <UserButton />
-    </SignedIn>
-    <ModeToggle />
+     <h1>Home page content</h1>
     </div>
   );
 }
