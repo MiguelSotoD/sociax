9ac92eabec479315bb64b01e91d585e6e16525a7
MiguelSotoD <23soto330@gmail.com>
2025-05-01 19:36:09 -0600

    Adding sheet components and updating the mobile navigation bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
                 {/* Contenedor para centrar el contenido */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3"></div>
+                    <div className="hidden lg:block lg:col-span-3">Sidebar</div>
                     <div className="lg:col-span-9">
                       {children}  
                     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "lucide-react"
 import DesktopNavbar from "./DesktopNavbar"
+import MobileNavbar from "./MobileNavbar"
 
 function Navbar() {
   return (
@@ -13,7 +14,7 @@ function Navbar() {
           </div>
 
           <DesktopNavbar />
-          {/* <MobileNavbar /> */}
+          <MobileNavbar />
         </div>
       </div>
     </nav>
